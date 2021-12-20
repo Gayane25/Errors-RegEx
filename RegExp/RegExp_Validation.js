6af29ba9037d3e94d8edc5a1362ee88ffb94743d
Gayane25 <gayabennu@gmail.com>
2021-12-20 21:42:08 +0400

    feat: added strict mode and ticket in comment

---
+++ RegExp/RegExp_Validation.js
@@ -0,0 +1,6 @@
+'use strict';
+/*
+Create a RegExp myRegExp to test if a string is a valid pin or not. A valid pin has:
+Exactly 4 or 6 characters.
+Only numerical characters (0-9).
+No whitespace.*/
