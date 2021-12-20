67c9adff9d2f791a79d165f05981d93a7645cbff
Gayane25 <gayabennu@gmail.com>
2021-12-20 21:43:26 +0400

    feat: solved with test method

--- RegExp/RegExp_Validation.js
+++ RegExp/RegExp_Validation.js
@@ -4,3 +4,7 @@ Create a RegExp myRegExp to test if a string is a valid pin or not. A valid pin 
 Exactly 4 or 6 characters.
 Only numerical characters (0-9).
 No whitespace.*/
+
+let myRegExp = ' 4983';
+
+console.log(/(^(\d{4})$)|^(\d{6}$)/.test(myRegExp));
