c28b80939a8e5ea032f922db7fffd454ef018901
Gayane25 <gayabennu@gmail.com>
2021-12-20 21:35:47 +0400

    feat: solved with regular expression

--- RegExp/RegExp2.js
+++ RegExp/RegExp2.js
@@ -3,3 +3,6 @@
 let str = '<> <a href="/"> <input type="radio" checked> <b>';
 str.match(REGEXP);
 '<a href="/">', '<input type="radio" checked>', "<b>"; */
+
+let str = '<> <a href="/"> <input type="radio" checked> <b>';
+console.log(str.match(/[^'<>'].*/));
