57c6db3db0746ecaf5b5096949e695ecb84c7cb4
Gayane25 <gayabennu@gmail.com>
2021-12-20 19:21:32 +0400

    feat:ticket solved with try catch error method

--- Errors/reversedString.js
+++ Errors/reversedString.js
@@ -5,3 +5,13 @@
 . If an exception is thrown, catch it and print the contents of the exception's message on a new line.
 . Print s on new line. If no exception was thrown, then this should be the reversed string, if an
 exception was thrown, this should be the original string.*/
+
+function reverseString(s) {
+  try {
+    s = s.split('').reverse().join('');
+  } catch (err) {
+    console.log('s.split is not a function:' + err.name);
+  }
+  console.log(s);
+}
+reverseString(1234);
